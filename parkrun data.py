c7f701e01af0f6ecc3d23dc76ebc6726bff53823
josh-justjosh <54246291+josh-justjosh@users.noreply.github.com>
2021-06-28 12:25:59 +0100

    trying again

--- parkrun data.py
+++ parkrun data.py
@@ -8,10 +8,10 @@ from html.parser import HTMLParser
 import xml.etree.ElementTree as ET
 import twython
 
-consumer_key = '${{ secrets.TWITTER_CONSUMER_KEY }}'
-consumer_secret = '${{ secrets.TWITTER_CONSUMER_SECRET }}'
-access_token = '${{ secrets.TWITTER_ACCESS_TOKEN }}'
-access_token_secret = '${{ secrets.TWITTER_ACCESS_TOKEN_SECRET }}'
+consumer_key = ${{ secrets.TWITTER_CONSUMER_KEY }}
+consumer_secret = ${{ secrets.TWITTER_CONSUMER_SECRET }}
+access_token = ${{ secrets.TWITTER_ACCESS_TOKEN }}
+access_token_secret = ${{ secrets.TWITTER_ACCESS_TOKEN_SECRET }}
 
 from twython import Twython
 twitter = Twython(
