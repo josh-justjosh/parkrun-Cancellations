a2f3e7f0448cc935a3cb004a89c95013716ac4d6
josh-justjosh <54246291+josh-justjosh@users.noreply.github.com>
2021-07-22 07:48:04 +0100

    stops saving tei and cancellations html files

--- parkrun data.py
+++ parkrun data.py
@@ -64,15 +64,15 @@ with open('_data/parkrun/raw/events.json','wt', encoding='utf-8', newline='') as
 
 technical_event_info = requests.get('https://wiki.parkrun.com/index.php/Technical_Event_Information').text
 
-with open('_data/parkrun/raw/tei.html','wt', encoding='utf-8', newline='') as f:
-    f.write(technical_event_info)
-    print(now(),"raw/tei.html saved")
+#with open('_data/parkrun/raw/tei.html','wt', encoding='utf-8', newline='') as f:
+#    f.write(technical_event_info)
+#    print(now(),"raw/tei.html saved")
 
 cancellations = requests.get('https://wiki.parkrun.com/index.php/Cancellations/Global').text.replace("’","'")
 
-with open('_data/parkrun/raw/cancellations.html','wt', encoding='utf-8', newline='') as f:
-    f.write(cancellations)
-    print(now(),"raw/cancellations.html saved")
+#with open('_data/parkrun/raw/cancellations.html','wt', encoding='utf-8', newline='') as f:
+#    f.write(cancellations)
+#    print(now(),"raw/cancellations.html saved")
 
 events = json.loads(events)['events']
 
