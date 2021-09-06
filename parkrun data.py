86f89648c76f91f507cfab5ad942e74038045d42
josh-justjosh <54246291+josh-justjosh@users.noreply.github.com>
2021-09-06 01:47:56 +0100

    prevent overwriting of data

--- parkrun data.py
+++ parkrun data.py
@@ -1314,6 +1314,9 @@ def writehistory(file, data):
         if now().weekday() == 0 and now().hour <= 1:
             data['time'] = str(now())
             old_data = [old_data[-1],data]
+            olddate = datetime.datetime.strptime(old_data[-1]['time'], '%Y-%m-%d %H:%M:%S.%f')
+            if olddate.date() != now().date() and olddate.hour != now().hour:
+                old_data = [old_data[-1],data]
     except FileNotFoundError:
         old_data = []
     print(now(),"history/"+file+" read")
